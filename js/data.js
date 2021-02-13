4058ca52b74081f7a813f289f5b7593d02ecb8ce
Kianna Brandt <brandtkianna@gmail.com>
2021-02-13 11:42:25 -0800

    Updated the viewSwap function to include a loop.

--- js/data.js
+++ js/data.js
@@ -13,14 +13,12 @@ var data = {
 };
 
 var $profileForm = document.querySelector('#profile-form');
-var $editProfile = document.querySelector('#edit-profile');
 var $profile = document.querySelector('#profile');
 var $defaultImg = document.querySelector('.default-img');
 var $entriesLink = document.querySelector('#entries-link');
-var $entries = document.querySelector('#entries');
-var $createEntry = document.querySelector('#create-entry');
 var $entriesForm = document.querySelector('#entries-form');
 var $defaultEntryImg = document.querySelector('.default-entry-img');
+var $view = document.querySelectorAll('.view');
 
 $profileForm.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -99,12 +97,15 @@ function renderProfile() {
 }
 
 function viewSwap(dataView) {
+  for (var i = 0; i < $view.length; i++) {
+    if ($view[i].id === dataView) {
+      $view[i].className = '';
+      data.view = $view[i].id;
+    } else {
+      $view[i].className = 'hidden';
+    }
+  }
   if (dataView === 'edit-profile') {
-    $editProfile.className = '';
-    $profile.className = 'hidden';
-    $entries.className = 'hidden';
-    $createEntry.className = 'hidden';
-    data.view = 'edit-profile';
     $profileForm.elements.username.value = data.profile.username;
     $profileForm.elements.fullName.value = data.profile.fullName;
     $profileForm.elements.location.value = data.profile.location;
@@ -112,31 +113,18 @@ function viewSwap(dataView) {
     $profileForm.elements.bio.value = data.profile.bio;
     if (data.profile.username !== '') {
       $defaultImg.src = data.profile.avatarUrl;
+      $entriesLink.className = '';
+    }
+    if (data.profile.username === '') {
+      $entriesLink.className = 'hidden';
     }
   }
   if (dataView === 'profile') {
-    $editProfile.className = 'hidden';
-    $profile.className = '';
-    $entries.className = 'hidden';
-    $createEntry.className = 'hidden';
-    $entriesLink.className = '';
-    data.view = 'profile';
     $profile.removeChild($profile.childNodes[0]);
     $profile.appendChild(renderProfile());
   }
-  if (dataView === 'entries') {
-    $editProfile.className = 'hidden';
-    $profile.className = 'hidden';
-    $entries.className = '';
-    $createEntry.className = 'hidden';
-    data.view = 'entries';
-  }
-  if (dataView === 'create-entry') {
-    $editProfile.className = 'hidden';
-    $profile.className = 'hidden';
-    $entries.className = 'hidden';
-    $createEntry.className = '';
-    data.view = 'create-entry';
+  if (data.profile.username !== '') {
+    $entriesLink.className = '';
   }
 }
 
